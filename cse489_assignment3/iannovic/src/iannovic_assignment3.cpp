6d8e403c07efa9ca215ac38b281a61f56920fe88
ian <ian@ian-VirtualBox>
2014-11-11 15:31:11 -0500

    initial committ. here we go

---
+++ cse489_assignment3/iannovic/src/iannovic_assignment3.cpp
@@ -0,0 +1,50 @@
+/**
+ * @iannovic_assignment3
+ * @author  Ian Novickis <iannovic@buffalo.edu>
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * This contains the main function. Add further description here....
+ */
+#include <iostream>
+#include <stdio.h>
+
+#include "../include/global.h"
+#include "../include/logger.h"
+
+using namespace std;
+
+/**
+ * main function
+ *
+ * @param  argc Number of arguments
+ * @param  argv The argument list
+ * @return 0 EXIT_SUCCESS
+ */
+int main(int argc, char **argv)
+{
+	/*Init. Logger*/
+        cse4589_init_log();
+
+	/* Clear LOGFILE and DUMPFILE */
+        fclose(fopen(LOGFILE, "w"));
+        fclose(fopen(DUMPFILE, "wb"));
+
+	/*Start Here*/
+	
+	return 0;
+}
